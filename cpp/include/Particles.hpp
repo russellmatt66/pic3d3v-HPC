ce6703444bdc215c6109913c3b45a93d689b5ae3
russellmatt66 <47687321+russellmatt66@users.noreply.github.com>
2023-12-05 22:51:00 -0800

    Update Particles.hpp
    Finished writing boilerplate for particles

--- cpp/include/Particles.hpp
+++ cpp/include/Particles.hpp
@@ -8,15 +8,87 @@ class ParticleSpecies3d3v{
 	public:
 		// Constructor
 		ParticleSpecies3d3v(size_t N, size_t Q, string ID) :
-		species_id_(ID) , N_(N), Q_(Q), 
+		N_(N), Q_(Q), species_id_(ID), 
 		particle_x_(N, 0.0), particle_y_(N, 0.0), particle_z_(N, 0.0), 
 		particle_vx_(N, 0.0), particle_vy_(N, 0.0), particle_vz_(N, 0.0),
 		particle_Fx_(N, 0.0), particle_Fy_(N, 0.0), particle_Fz_(N, 0.0),
 		xfound_(N, 0), yfound_(N, 0), zfound_(N, 0)
 		{}
 		
-		// Boilerplate, abandon all hope of not reading accessor and getters, ye who enter here!
+		/* Boilerplate, abandon all hope of not reading accessors, ye who enter here! */
+		// Basic attributes
+        const size_t ParticleNum() const { return N_; }
+		const double Q() const { return Q_; }
+		const string SpeciesID() const { return species_id_; }
+		string SpeciesID() { return species_id_[i]; }
 		
+        // Element-wise accessor methods
+        const double ParticleX(size_t i) const { return particles_x_[i]; }
+		const double ParticleY(size_t i) const { return particles_y_[i]; }
+		const double ParticleZ(size_t i) const { return particles_z_[i]; }
+		
+        const double ParticleVx(size_t i) const { return particles_vx_[i]; }
+        const double ParticleVy(size_t i) const { return particles_vy_[i]; }
+		const double ParticleVz(size_t i) const { return particles_vz_[i]; }
+		
+		const double ParticleFx(size_t i) const {return particles_Fx_[i]; }
+		const double ParticleFy(size_t i) const {return particles_Fy_[i]; }
+		const double ParticleFz(size_t i) const {return particles_Fz_[i]; }
+        
+		const size_t XFound(size_t i) const { return xfound_[i]; }
+		const size_t YFound(size_t i) const { return yfound_[i]; }
+		const size_t ZFound(size_t i) const { return zfound_[i]; }
+
+        double &ParticleX(size_t i) { return particles_x_[i]; }
+		double &ParticleY(size_t i) { return particles_y_[i]; }
+		double &ParticleZ(size_t i) { return particles_z_[i]; }
+		
+        double &ParticleVx(size_t i) { return particles_vx_[i]; }
+		double &ParticleVy(size_t i) { return particles_vy_[i]; }
+		double &ParticleVz(size_t i) { return particles_vz_[i]; }
+        
+		double &ParticleFx(size_t i) { return particles_Fx_[i]; }
+		double &ParticleFy(size_t i) { return particles_Fy_[i]; }
+		double &ParticleFz(size_t i) { return particles_Fz_[i]; }
+        
+		size_t &XFound(size_t i) { return xfound_[i]; }
+		size_t &YFound(size_t i) { return yfound_[i]; }
+		size_t &ZFound(size_t i) { return zfound_[i]; }
+
+
+        // Also need to be able to access whole data members
+        const std::vector<double>& getParticleX() const { return particles_x_; }
+		const std::vector<double>& getParticleY() const { return particles_y_; }
+		const std::vector<double>& getParticleZ() const { return particles_z_; }
+        
+		const std::vector<double>& getParticleVx() const { return particles_vx_; }
+		const std::vector<double>& getParticleVy() const { return particles_vy_; }
+		const std::vector<double>& getParticleVz() const { return particles_vz_; }
+        
+		const std::vector<double>& getParticleFx() const { return particles_Fx_; }
+		const std::vector<double>& getParticleFy() const { return particles_Fy_; }
+		const std::vector<double>& getParticleFz() const { return particles_Fz_; }
+        
+		const std::vector<size_t>& getXFound() const { return xfound_; }
+		const std::vector<size_t>& getYFound() const { return yfound_; }
+		const std::vector<size_t>& getZFound() const { return zfound_; }
+
+
+        std::vector<double>& getParticleX() { return particles_x_; }
+		std::vector<double>& getParticleY() { return particles_y_; }
+		std::vector<double>& getParticleZ() { return particles_z_; }
+		
+        std::vector<double>& getParticleVx() { return particles_vx_; }
+		std::vector<double>& getParticleVy() { return particles_vy_; }
+		std::vector<double>& getParticleVz() { return particles_vz_; }
+		
+        std::vector<double>& getParticleFx() { return particles_Fx_; }
+		std::vector<double>& getParticleFy() { return particles_Fy_; }
+		std::vector<double>& getParticleFz() { return particles_Fz_; }
+		
+        std::vector<size_t>& getXFound() { return xfound_; }
+		std::vector<size_t>& getYFound() { return yfound_; }
+		std::vector<size_t>& getZFound() { return zfound_; }
 		
 	private:
 		string species_id_;
@@ -39,46 +111,107 @@ class ParticleSpecies3d3v{
 // Class to collect all the particles together
 class ParticleList{
     public:
-        ParticleList(size_t N) : 
-        N_(N), particles_x(N, 0.0), particles_vx(N, 0.0), particles_Ex(N, 0.0), x_found(N, 0), species_id(N, "") 
+		ParticleList(size_t N, std::vector<ParticleSpecies3d3v> SpeciesArr) : 
+        N_(N), Q_(N, 0.0), species_id(N, "") 
+		particles_x(N, 0.0), particles_y_(N, 0.0), particles_z_(N, 0.0),
+		particles_vx(N, 0.0), particles_vy_(N, 0.0), particles_vz_(N, 0.0),
+		particles_Fx(N, 0.0), particles_Fy_(N, 0.0), particles_Fz_(N, 0.0),
+		x_found_(N, 0), yfound_(N, 0), zfound_(N, 0)
+
         {}
 
-        // Boilerplate
-        const size_t getParticleNum() const { return N_; }
+        /* Boilerplate, abandon all hope of not reading accessors, ye who enter here! */
+		// Basic attributes
+        const size_t ParticleNum() const { return N_; }
+		const double Q(size_t i) const { return Q_[i]; }
+		const string SpeciesID(size_t i) const { return species_id_[i]; }
+		string SpeciesID(size_t i) { return species_id_[i]; }
+		
+		const std::vector<string>& getSpeciesID() const { return species_id_; }
+		std::vector<string>& getSpeciesID() { return species_id_; }
 
         // Element-wise accessor methods
-        const double ParticleX(size_t i) const { return particles_x[i]; }
-        const double ParticleVx(size_t i) const { return particles_vx[i]; }
-        const double ParticleEx(size_t i) const {return particles_Ex[i]; }
-        const size_t XFound(size_t i) const { return x_found[i]; }
-		const std::string SpeciesID(size_t i) const { return species_id[i]; }
+        const double ParticleX(size_t i) const { return particles_x_[i]; }
+		const double ParticleY(size_t i) const { return particles_y_[i]; }
+		const double ParticleZ(size_t i) const { return particles_z_[i]; }
+		
+        const double ParticleVx(size_t i) const { return particles_vx_[i]; }
+        const double ParticleVy(size_t i) const { return particles_vy_[i]; }
+		const double ParticleVz(size_t i) const { return particles_vz_[i]; }
+		
+		const double ParticleFx(size_t i) const {return particles_Fx_[i]; }
+		const double ParticleFy(size_t i) const {return particles_Fy_[i]; }
+		const double ParticleFz(size_t i) const {return particles_Fz_[i]; }
+        
+		const size_t XFound(size_t i) const { return xfound_[i]; }
+		const size_t YFound(size_t i) const { return yfound_[i]; }
+		const size_t ZFound(size_t i) const { return zfound_[i]; }
 
-        double &ParticleX(size_t i) { return particles_x[i]; }
-        double &ParticleVx(size_t i) { return particles_vx[i]; }
-        double &ParticleEx(size_t i) { return particles_Ex[i]; }
-        size_t &XFound(size_t i) { return x_found[i]; }
-		std::string SpeciesID(size_t i) { return species_id[i]; }
+        double &ParticleX(size_t i) { return particles_x_[i]; }
+		double &ParticleY(size_t i) { return particles_y_[i]; }
+		double &ParticleZ(size_t i) { return particles_z_[i]; }
+		
+        double &ParticleVx(size_t i) { return particles_vx_[i]; }
+		double &ParticleVy(size_t i) { return particles_vy_[i]; }
+		double &ParticleVz(size_t i) { return particles_vz_[i]; }
+        
+		double &ParticleFx(size_t i) { return particles_Fx_[i]; }
+		double &ParticleFy(size_t i) { return particles_Fy_[i]; }
+		double &ParticleFz(size_t i) { return particles_Fz_[i]; }
+        
+		size_t &XFound(size_t i) { return xfound_[i]; }
+		size_t &YFound(size_t i) { return yfound_[i]; }
+		size_t &ZFound(size_t i) { return zfound_[i]; }
+
 
         // Also need to be able to access whole data members
-        const std::vector<double>& getParticleX() const { return particles_x; }
-        const std::vector<double>& getParticleVx() const { return particles_vx; }
-        const std::vector<double>& getParticleEx() const { return particles_Ex; }
-        const std::vector<size_t>& getXFound() const { return x_found; }
-		const std::vector<std::string>& getSpeciesID() const { return species_id; }
+        const std::vector<double>& getParticleX() const { return particles_x_; }
+		const std::vector<double>& getParticleY() const { return particles_y_; }
+		const std::vector<double>& getParticleZ() const { return particles_z_; }
+        
+		const std::vector<double>& getParticleVx() const { return particles_vx_; }
+		const std::vector<double>& getParticleVy() const { return particles_vy_; }
+		const std::vector<double>& getParticleVz() const { return particles_vz_; }
+        
+		const std::vector<double>& getParticleFx() const { return particles_Fx_; }
+		const std::vector<double>& getParticleFy() const { return particles_Fy_; }
+		const std::vector<double>& getParticleFz() const { return particles_Fz_; }
+        
+		const std::vector<size_t>& getXFound() const { return xfound_; }
+		const std::vector<size_t>& getYFound() const { return yfound_; }
+		const std::vector<size_t>& getZFound() const { return zfound_; }
 
-        std::vector<double>& getParticleX() { return particles_x; }
-        std::vector<double>& getParticleVx() { return particles_vx; }
-        std::vector<double>& getParticleEx() { return particles_Ex; }
-        std::vector<size_t>& getXFound() { return x_found; }
-		std::vector<std::string>& getSpeciesID() { return species_id; }
+
+        std::vector<double>& getParticleX() { return particles_x_; }
+		std::vector<double>& getParticleY() { return particles_y_; }
+		std::vector<double>& getParticleZ() { return particles_z_; }
+		
+        std::vector<double>& getParticleVx() { return particles_vx_; }
+		std::vector<double>& getParticleVy() { return particles_vy_; }
+		std::vector<double>& getParticleVz() { return particles_vz_; }
+		
+        std::vector<double>& getParticleFx() { return particles_Fx_; }
+		std::vector<double>& getParticleFy() { return particles_Fy_; }
+		std::vector<double>& getParticleFz() { return particles_Fz_; }
+		
+        std::vector<size_t>& getXFound() { return xfound_; }
+		std::vector<size_t>& getYFound() { return yfound_; }
+		std::vector<size_t>& getZFound() { return zfound_; }
 
     private:
-        size_t N_;
-        std::vector<double> particles_x;
-		std::vector<double> particles_y;
-        std::vector<double> particles_z;
-        std::vector<double> particles_vx;
-        std::vector<double> particles_Ex;
-        std::vector<size_t> x_found;
-        std::vector<std::string> species_id; 
+		size_t N_;
+        std::vector<double> Q_;
+		std::vector<string> species_id_;
+        std::vector<double> particle_x_;
+		std::vector<double> particle_y_;
+		std::vector<double> particle_z_;
+        std::vector<double> particle_vx_;
+        std::vector<double> particle_vy_;
+        std::vector<double> particle_vz_;
+        std::vector<double> particle_Fx_;
+        std::vector<double> particle_Fy_;
+        std::vector<double> particle_Fz_;
+        std::vector<size_t> xfound_;
+        std::vector<size_t> yfound_;
+        std::vector<size_t> zfound_;
 };
